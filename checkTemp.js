e35209bd700e03f5a77c66d43dfdb8dc76caac7f
KevinKallo <90027927+KevinKallo@users.noreply.github.com>
2021-09-24 12:11:33 +0300

    Update and rename check to checkTemp.js

---
+++ checkTemp.js
@@ -0,0 +1,16 @@
+
+function checkTemp(temp) {
+    console.log("Temp on "+temp)
+
+if ( temp >= 21 && temp <= 40 ) {
+    return 0;
+
+} if ( temp > 0 && temp <= 20 ) {
+    return -1;
+
+} else if ( temp > 40 && temp < 60) 
+    return 1;
+
+
+}
+© 2021 GitHub, Inc.
